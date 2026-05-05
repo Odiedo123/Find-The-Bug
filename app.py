865268cd44ec819e942607b9fffeb62ee5d8a0df
Jed Oloo Odundo <Jed@student.aoa>
2026-05-05 18:14:20 +0200

    Uncommenting AI and using my 20 free prompts cause im broke

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ load_dotenv("keys.env")
 
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
-#GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
 
 client = genai.Client()
@@ -17,28 +17,28 @@ code_snippet = [""]
 
 @app.route("/")
 def home_page():
-    # user_input = "Give me 30 lines of C++ code"
-    # response = client.models.generate_content(
-    #     model="gemini-3-flash-preview",
-    #     config=types.GenerateContentConfig(
-    #     system_instruction="Give code without comments and make the code have one bug somewhere"),
-    #     contents=user_input
-    # )
-    # code_snippet[0] = response.text
+    user_input = "Give me 30 lines of C++ code"
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        config=types.GenerateContentConfig(
+        system_instruction="Give code without comments and make the code have one bug somewhere but use code at the level of IOI and PAn African Informatics Olympiad"),
+        contents=user_input
+    )
+    code_snippet[0] = response.text
 
-    return render_template('index.html')#, text=response.text)
+    return render_template('index.html', text=response.text)
 
 @app.route('/submit-bug', methods=['POST'])
 def submit_bug():
-    # data = request.get_json()
-    # report = data.get('description')
-    # response = client.models.generate_content(
-    #     model="gemini-3-flash-preview",
-    #     config=types.GenerateContentConfig(
-    #     system_instruction="Explain the bug in the code in detail in about 20 lines and state if the bug as described by the following text is correct"),
-    #     contents=code_snippet[0] + " " + report
-    # )
-    return {"status": "Success", "output": "woo"}#response.text}
+    data = request.get_json()
+    report = data.get('description')
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        config=types.GenerateContentConfig(
+        system_instruction="Explain the bug in the code in detail in about 20 lines and state if the bug as described by the following text is correct. Also format the response such that when pasted directly its is organized with enter spaces and all."),
+        contents=code_snippet[0] + " " + report
+    )
+    return {"status": "Success", "output": response.text}
 
 if __name__ == '__main__':
     app.run(debug=True)
